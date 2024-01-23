edccab869a839ace7917a02099e519306ba61570
Thomas Garayua <tgarayua@gmail.com>
2024-01-22 21:30:40 -0500

    Created InputView, LoginView and RegistrationView

---
+++ Rendezvous/View/Authentfication/RegistrationView.swift
@@ -0,0 +1,86 @@
+//
+//  RegistrationView.swift
+//  Rendezvous
+//
+//  Created by Thomas Garayua on 1/22/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "mappin.and.ellipse")
+                .font(.system(size: 80))
+                .foregroundColor(.orange)
+                .scaledToFill()
+                .frame(width: 100, height: 120)
+                .padding(.vertical, 32)
+            
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                    .autocapitalization(.none)
+                
+                InputView(text: $fullName,
+                          title: "Full Name",
+                          placeholder: "John Doe")
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
+            
+            Button {
+                print("Sign user up...")
+            } label: {
+                HStack {
+                    Text("SIGN UP")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            }
+            .background(Color(.systemBlue))
+            .cornerRadius(10)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                        
+                }
+                .font(.system(size: 14))
+            }
+
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
